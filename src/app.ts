8ac15b2b0fa03c5c0329ee06e386558441dfc564
shantanucultureholidays <shantanu.cultureholidays@gmail.com>
2025-04-27 11:19:30 +0530

    netlify

--- src/app.ts
+++ src/app.ts
@@ -62,7 +62,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // corsUrl
+    origin: ['http://localhost:5173', 'http://localhost:5174','https://zenchats.netlify.app'], // corsUrl
     optionsSuccessStatus: 200,
     credentials: true,
   })
@@ -95,7 +95,8 @@ const io = new SocketServer(httpServer, {
     origin: [
       "http://localhost:5173",  
       "http://localhost:3002",
-      "http://localhost:5174" 
+      "http://localhost:5174" ,
+      "https://zenchats.netlify.app"
     ],
     methods: ["GET", "POST"],
     credentials: true,
